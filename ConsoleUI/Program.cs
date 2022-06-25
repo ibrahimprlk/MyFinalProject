beebabbaa34103ac14c9fddf2a3f3b70363414d1
İbrahim Parlak <ibrahimparlak3694@gmail.com>
2022-06-25 23:32:27 +0300

    Projeye başlandı

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,18 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new InMemoryProductDal());
+            foreach (var x in productManager.GetAll())
+            {
+                Console.WriteLine(x.ProductId+" "+x.CategoryId+" "+x.ProductName+" "+x.UnitPrice+" "+x.UnitsInStock);
+            }
+        }
+    }
+}
